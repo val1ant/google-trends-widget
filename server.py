dd930e4c799ae2c03df165d9c9a3029a305747e8
rahulbot <rahulbot@gmail.com>
2015-08-07 09:53:24 -0400

    use static path for loading config file
    so it works on the server

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
+import os
 from flask import Flask, url_for, request, render_template, jsonify, abort
 from apiclient.discovery import build 
 import json
+from ConfigParser import SafeConfigParser
 
 DEBUG = True
 
-from ConfigParser import SafeConfigParser
+basedir = os.path.dirname(os.path.abspath(__file__))
 parser = SafeConfigParser()
-parser.read('config.txt')
+parser.read(os.path.join(basedir,'config.txt'))
+
 MY_DEVELOPER_KEY = parser.get('KEY','MY_DEVELOPER_KEY')
 SERVER = 'https://www.googleapis.com'
 DISCOVERY_URL_SUFFIX = '/discovery/v1/apis/trends/v1beta/rest'
